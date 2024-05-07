e0830ef2e1f589be334c077a81a838679f5ef651
jokee <qjoel44@yahoo.com>
2024-05-06 23:34:14 -0500

    update store with devtools and persist

--- todo-zustand/src/store/todoStore.js
+++ todo-zustand/src/store/todoStore.js
@@ -1,12 +1,17 @@
 import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
 // * Crear un 'store' con zustand (Hooks)
-const useTodoStore = create(set => ({ //Based on fucntions
+const useTodoStore = create(devtools(set => ({ //Based on fucntions
     todos: [], //Array of todos
     // Set function updates the state. Argument (state) is the current state and returns the new state
     addTodo: (text) => set((state) => ({ todos: [...state.todos, {id: Date.now(), text, completed: false}] })), 
     removeTodo: (id) => set((state) => ({ todos: state.todos.filter(t => t.id !== id) })), 
     markTodo: (id) => set((state) => ({ todos: state.todos.map(t => t.id === id ? {...t, completed: !t.completed} : t) })), 
-}));
+})));
+
+/* let store; //Store
+store = devtools(store); //Devtools middleware
+store = persist(store, { name: "todos" }); //Persist middleware (Save the state in the local storage */
 
 export default useTodoStore;
