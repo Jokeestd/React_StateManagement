505119be5e7f1dda3098e3eb9e47362658e0f535
jokee <qjoel44@yahoo.com>
2024-05-06 19:16:31 -0500

    Fix store

--- todo-zustand/src/store/todoStore.js
+++ todo-zustand/src/store/todoStore.js
@@ -6,7 +6,7 @@ const useTodoStore = create(set => ({ //Based on fucntions
     // Set function updates the state. Argument (state) is the current state and returns the new state
     addTodo: (text) => set((state) => ({ todos: [...state.todos, {id: Date.now(), text, completed: false}] })), 
     removeTodo: (id) => set((state) => ({ todos: state.todos.filter(t => t.id !== id) })), 
-    markTodo: (id) => set((state) => ({ todos: state.todos.map(todo => t.id === id ? {...t, completed: !t.completed} : t) })), 
+    markTodo: (id) => set((state) => ({ todos: state.todos.map(t => t.id === id ? {...t, completed: !t.completed} : t) })), 
 }));
 
 export default useTodoStore;
